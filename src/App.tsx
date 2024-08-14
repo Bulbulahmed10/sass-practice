3bc7973822db5cbf9d916a081fce188c9b3f361d
Bulbul Ahmed <mdbulbulmolla1222@gmail.com>
2024-08-14 17:32:46 +0600

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './index.scss'
+import Header from './components/Header/Header'
 const App: React.FC = () => {
   return (
     <div>
+      <Header />
       <h1>this is react project</h1>
     </div>
   )
